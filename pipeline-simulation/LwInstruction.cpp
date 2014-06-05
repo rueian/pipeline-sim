a1608507d764ee1a290e2b1ff80ac3631f3d023b
Rueian Oneecom <rueiancsie@gmail.com>
2014-06-05 10:38:57 +0800

    simpilify pipeline registers assignment

--- pipeline-simulation/LwInstruction.cpp
+++ pipeline-simulation/LwInstruction.cpp
@@ -11,28 +11,36 @@ LwInstruction::LwInstruction(string machineCode)
 }
 
 void LwInstruction::IDStage() {
-    _regs->plReg["ID/EX"]["Rs"] = to_string(_rs);
-    _regs->plReg["ID/EX"]["Rt"] = to_string(_rt);
-    _regs->plReg["ID/EX"]["sign_ext"] = to_string(_immediate);
-    _regs->plReg["ID/EX"]["Control Signals"] = _controlSginal;
-
-    _regs->plReg["ID/EX"]["ReadData1"] = to_string((int)_regs->reg[_rs]);
-    _regs->plReg["ID/EX"]["ReadData2"] = to_string((int)_regs->reg[_rt]);
+    _regs->plReg["ID/EX"] = {
+            {"ReadData1", to_string(_regs->reg[_rs])},
+            {"ReadData2", to_string(_regs->reg[_rt])},
+            {"sign_ext", to_string(_immediate)},
+            {"Rs", to_string(_rs)},
+            {"Rt", to_string(_rt)},
+            {"Rd", "0"},
+            {"Control Signals", _controlSginal}
+    };
 }
 
 void LwInstruction::EXStage() {
     int readData1 = stoi(_regs->plReg["ID/EX"]["ReadData1"]);
     int sign_ext = stoi(_regs->plReg["ID/EX"]["sign_ext"]);
-    _regs->plReg["EX/MEM"]["ALUout"] = to_string(readData1 + sign_ext);
-    _regs->plReg["EX/MEM"]["Rt"] = _regs->plReg["ID/EX"]["Rt"];
-    _regs->plReg["EX/MEM"]["Rd"] = "";
-    _regs->plReg["EX/MEM"]["Control Signals"] = _controlSginal.substr(4, 5);
+
+    _regs->plReg["EX/MEM"] = {
+            {"ALUout", to_string(readData1 + sign_ext)},
+            {"WriteData", "0"},
+            {"Rt", _regs->plReg["ID/EX"]["Rt"]},
+            {"Rd", ""},
+            {"Control Signals", _controlSginal.substr(4, 5)}
+    };
 }
 
 void LwInstruction::MEMStage() {
-    _regs->plReg["MEM/WB"]["ReadData"] = to_string(_memory->getDataMemory(_regs->plReg["EX/MEM"]["ALUout"]));
-    _regs->plReg["MEM/WB"]["ALUout"] = _regs->plReg["EX/MEM"]["ALUout"];
-    _regs->plReg["MEM/WB"]["Control Signals"] = _controlSginal.substr(7, 2);
+    _regs->plReg["MEM/WB"] = {
+            {"ReadData", to_string(_memory->getDataMemory(_regs->plReg["EX/MEM"]["ALUout"]))},
+            {"ALUout", _regs->plReg["EX/MEM"]["ALUout"]},
+            {"Control Signals", _controlSginal.substr(7, 2)}
+    };
 }
 
 void LwInstruction::WBStage() {
